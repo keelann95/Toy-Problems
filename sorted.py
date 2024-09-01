ce7d1d0d5ae3490a78b7a50d769a86bab9f59e9b
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-09-01 11:41:39 +0300

    created a sorted file

---
+++ sorted.py
@@ -0,0 +1,10 @@
+def sort_by_age(tuples_list):
+    return sorted(tuples_list, key=lambda x: x[1])
+
+def sort_by_age(tuples_list):
+    return sorted(tuples_list, key=lambda x: x[1])
+
+# Example usage:
+people = [("Alice", 30), ("Bob", 25), ("Charlie", 35)]
+sorted_people = sort_by_age(people)
+print(sorted_people)
